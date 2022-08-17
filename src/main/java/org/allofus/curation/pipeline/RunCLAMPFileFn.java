9978c024cfce3f8b908abf559c08dd866bdcb18e
Jianfu Li <lijianfu2008@gmail.com>
2022-08-17 13:51:58 -0500

    update with clamp pipeline with input from file

--- src/main/java/org/allofus/curation/pipeline/RunCLAMPFileFn.java
+++ src/main/java/org/allofus/curation/pipeline/RunCLAMPFileFn.java
@@ -7,11 +7,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.DoFn.Element;
+import org.apache.beam.sdk.transforms.DoFn.OutputReceiver;
+import org.apache.beam.sdk.transforms.DoFn.ProcessElement;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -37,59 +46,50 @@ import edu.uth.clamp.nlp.uima.UmlsEncoderUIMA;
 import edu.uth.clamp.nlp.structure.Document;
 
 
-public class RunCLAMPPipeline {
+public class RunCLAMPFileFn extends DoFn< FileIO.ReadableFile, String>  {
+//public class RunCLAMPFn extends DoFn<String, String>  {
+	static File inPath;
 	static List<DocProcessor> procList = new ArrayList<DocProcessor>();
 	static List<File> inputList = new ArrayList<File>();
 	static File outPath = null;
 	static File umlsIndex = null;
-		
-	public static int init_clamp( String[] argv ) throws ParseException, InvalidXMLException, ResourceInitializationException, org.apache.commons.cli.ParseException {
-		
-    	CommandLineParser parser = new BasicParser();
-		Options options = new Options();    	
-
-		options.addOption("i", "inputPath", true, "inputPath");
-		options.addOption("o", "outputPath", true, "outputPath");
-		options.addOption("p", "piepline", true, "clamp pipeline file");
-        options.addOption("I", "umlsIndex", true, "umls index dir");
-		
-		CommandLine commandLine = parser.parse(options, argv);
-		String inDir = "";
-		if (commandLine.hasOption("i")) {
-            inDir = commandLine.getOptionValue("i");
-		}
-		
-		String outDir = "";
-		if (commandLine.hasOption("o")) {
-            outDir = commandLine.getOptionValue("o");
-		}
-		
-		String pipeline_fn = "";
-		if (commandLine.hasOption("p")) {
-            pipeline_fn = commandLine.getOptionValue("p");
-		}
-		
-		String umlsIndexDir = "";
-    	if (commandLine.hasOption("I")) {
-	            umlsIndexDir = commandLine.getOptionValue("I");
-		}
 	
-
+	//static String outDir;
+	//static String pipeline_file;
+	//static String umlsIndexDir;
+			
+	public static int init_clamp( CurationNLPOptions options ) {
+		Instant start = Instant.now();	
+		String inDir = options.getInput();	
+		String outDir = options.getOutput();	
+		String pipeline_file = options.getPipeline();	
+		String umlsIndexDir = options.getUmls_index();
+        System.out.println(inDir);
+        System.out.println(outDir);
+        System.out.println(pipeline_file);
+        System.out.println(umlsIndexDir);
 		// check the input folder;		
-		File inPath = new File( inDir );
+		inPath = new File( inDir );
 		if( !inPath.exists() ) {
 			System.out.println( "input folder doesn't exist. input=[" + inDir + "]" );
 			return -1;
-		} else if( !inPath.isDirectory() ) {
-			System.out.println( "input should be a folder. input=[" + inDir + "]" );
-			return -1;
-		}
-		for( File file : inPath.listFiles() ) {
-			if( file.getName().startsWith( "." ) || !file.getName().endsWith( ".txt" ) ) {
-				continue;
+		} else if( inPath.isDirectory() ) {
+			//System.out.println( "input should be a folder. input=[" + inDir + "]" );
+			//return -1;
+			for( File file : inPath.listFiles() ) {
+				if( file.getName().startsWith( "." ) || !file.getName().endsWith( ".txt" ) ) {
+					continue;
+				} else {
+					inputList.add( file ); 
+				}
+			}			
+		} else {
+			if( inDir.startsWith( "." ) || !inDir.endsWith( ".txt" ) ) {
+				System.out.println( "Input should be a folder or a file endswith .txt. input=[" + inDir + "]" );
+				return -1;				
 			} else {
-				inputList.add( file ); 
-			}
+				inputList.add( inPath ); 
+			}			
 		}
 		System.out.println( "input files count=[" + inputList.size() + "]");
 
@@ -113,12 +113,12 @@ public class RunCLAMPPipeline {
 		}
 		
 		// pipeline
-		File pipelineJar = new File( pipeline_fn );
+		File pipelineJar = new File( pipeline_file );
 		if( !pipelineJar.exists() ) {
-			System.out.println( "pipeline jar doesn't exist. pipelineJar=[" + pipeline_fn + "]" );
+			System.out.println( "pipeline jar doesn't exist. pipelineJar=[" + pipeline_file + "]" );
 			return -1;
 		} else if( !pipelineJar.isFile() ) {
-			System.out.println( "pipeline jar should be a jar file or a .pipefile. pipelineJar=[" + pipeline_fn + "]" );
+			System.out.println( "pipeline jar should be a jar file or a .pipefile. pipelineJar=[" + pipeline_file + "]" );
 			return -1;
 		}
 		List<DocProcessor> pipeline = null;
@@ -129,7 +129,7 @@ public class RunCLAMPPipeline {
 			} else if( pipelineJar.getName().endsWith( ".jar" ) ) {
 				pipeline = ConfigUtil.importPipelineFromJar( pipelineJar );
 			} else {
-				System.out.println( "pipeline jar should be a jar file or a .pipefile. pipelineJar=[" + pipeline_fn + "]" );
+				System.out.println( "pipeline jar should be a jar file or a .pipefile. pipelineJar=[" + pipeline_file + "]" );
 				return -1;
 			}
 		
@@ -169,76 +169,59 @@ public class RunCLAMPPipeline {
 			return -1;
 		}
 		
+    	Instant end = Instant.now();
+    	Duration timeElapsed = Duration.between(start, end);
+    	System.out.println("init CLAMP: Time taken: "+ timeElapsed.toMillis() +" milliseconds");    	
 		
 		return 0;
 	}
 	
-	public static void run_pipeline() {   	
-    	for( File file : inputList ) {
-    		try {
-	    		Document doc = new Document( file );
-	    		for( DocProcessor proc : procList ) {
-	    			try {
-						proc.process( doc );
-					} catch (AnalysisEngineProcessException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-	    		}
-	    		// java File object
-	    		doc.save( outPath.getAbsolutePath() + File.separator + file.getName().replace( ".txt", ".xmi" ) );
-    		
-				FileWriter outfile = new FileWriter( new File( outPath.getAbsolutePath() + File.separator + file.getName() ) );
-				for( ClampNameEntity cne : doc.getNameEntity() ) {
-					outfile.write( "NamedEntity\t" + cne.getBegin() + "\t" + cne.getEnd() + "\tsemantic=" + cne.getSemanticTag() );
-					if( cne.getAssertion() != null && !cne.getAssertion().isEmpty() ) {
-						outfile.write( "\tassertion=" + cne.getAssertion() );
-					}
-					if( cne.getUmlsCui() != null && !cne.getUmlsCui().isEmpty() ) {
-						outfile.write( "\tcui=" + cne.getUmlsCui() );
-					}
-					/*if( cne.getAttr1() != null && !cne.getAttr1().isEmpty() ) {
-						outfile.write( "\tattr1=" + cne.getAttr1() );
-					}
-					if( cne.getAttr2() != null && !cne.getAttr2().isEmpty() ) {
-						outfile.write( "\tattr2=" + cne.getAttr2() );
-					}
-					if( cne.getAttr3() != null && !cne.getAttr3().isEmpty() ) {
-						outfile.write( "\tattr3=" + cne.getAttr3() );
-					}
-					if( cne.getAttr4() != null && !cne.getAttr4().isEmpty() ) {
-						outfile.write( "\tattr4=" + cne.getAttr4() );
-					}
-					if( cne.getAttr( "sentence_prob" ) != null ) {
-						outfile.write( "\tsentProb=" + cne.getAttr( "sentence_prob" ) );
-					}
-					if( cne.getAttr( "concept_prob" ) != null ) {
-						outfile.write( "\tconceptProb=" + cne.getAttr( "concept_prob" ) );
-					}*/
-					outfile.write( "\tne=" + cne.textStr().replace( "\r\n", " " ).replace( "\n", " ") );
-					outfile.write( "\n" );
-				}
-				
+    @ProcessElement
+    //public void processElement(@Element String element, OutputReceiver<String> receiver) {
+    public void processElement(@Element FileIO.ReadableFile file, OutputReceiver<String> receiver) throws DocumentIOException {
+    	try {
+    		    String filename = file.getMetadata().resourceId().getFilename().toString();
+    			Document doc = new Document( inPath.getAbsolutePath() + File.separator + filename );
+    			for( DocProcessor proc : procList ) {
+    				try {
+    					proc.process( doc );
+    				} catch (AnalysisEngineProcessException e) {
+    					// TODO Auto-generated catch block
+    					e.printStackTrace();
+    				}
+    			}
+    			// java File object
+    			doc.save( outPath.getAbsolutePath() + File.separator + filename.replace( ".txt", ".xmi" ) );
 
-				outfile.close();
-			} catch (IOException | UIMAException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (DocumentIOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    		
-    		System.out.println( file.getName() + " processed.." );
+    			FileWriter outfile = new FileWriter( new File( outPath.getAbsolutePath() + File.separator + filename ) );
+    			for( ClampNameEntity cne : doc.getNameEntity() ) {
+    				if( cne.getUmlsCui() != null && !cne.getUmlsCui().isEmpty() ) {
+    					receiver.output( "\tcui=" + cne.getUmlsCui() );
+    					outfile.write("\tcui=" + cne.getUmlsCui());
+    				}
+    				receiver.output( "\tne=" + cne.textStr().replace( "\r\n", " " ).replace( "\n", " ") );
+    				outfile.write("\tne=" + cne.textStr().replace( "\r\n", " " ).replace( "\n", " "));
+    				outfile.write( "\n" );
+    			}
+
+
+    			outfile.close();
+    	} catch (IOException | UIMAException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
     	}
-
+        System.out.println(receiver);
+    	System.out.println( "document processed.." );
+    }
 		
-	}
     
     public static void main( String[] argv ) throws InvalidXMLException, ResourceInitializationException, ParseException, org.apache.commons.cli.ParseException {
              // -i %input% -o %output% -p %pipeline% -A %umlsAPI% -I  %umlsIndex%
-		init_clamp(argv);
-        run_pipeline();
+        CurationNLPOptions options =
+                PipelineOptionsFactory.fromArgs(argv).withValidation().as(CurationNLPOptions.class);
+    	
+		init_clamp(options);
+        //run_pipeline();
         System.out.println("Run CLAMP pipeline done.");
     }
 }
