08f4e890680efcec438bfb3b035f2a1a9238f0c2
Nishanth Pavinkurve <npp267@nyu.edu>
2022-10-22 23:02:38 -0400

    Add io factory to dynamically select io

---
+++ src/main/java/io/factory/IOReadFactory.java
@@ -0,0 +1,16 @@
+package io.factory;
+
+import io.csv.CSVRead;
+import io.jsonl.JSONLRead;
+
+public class IOReadFactory {
+  public static IORead create(String input_type) {
+    if ("csv".equalsIgnoreCase(input_type)) {
+      return new CSVRead();
+    } else if ("jsonl".equalsIgnoreCase(input_type)) {
+      return new JSONLRead();
+    } // else if ("bigquery".equalsIgnoreCase(input_type)) {
+    //  return new BigQueryRead();
+    return null;
+  }
+}
