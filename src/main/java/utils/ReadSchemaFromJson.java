3eb171a831c3b27bae0d49a377a58ebd8f95b696
Nishanth Pavinkurve <npp267@nyu.edu>
2022-10-25 14:20:09 -0400

    Add helper for schema json string

--- src/main/java/utils/ReadSchemaFromJson.java
+++ src/main/java/utils/ReadSchemaFromJson.java
@@ -14,37 +14,43 @@ import java.util.List;
 
 public class ReadSchemaFromJson {
 
+  static String project_home = System.getProperty("user.dir");
+  static String resources_dir = project_home + "/src/main/resources";
+  static String schema_path = resources_dir + "/schemas/cdm/clinical/";
+
   public static Schema ReadSchema(String file_path) {
     List<Schema.Field> fields = new LinkedList<>();
-    try {
-      String project_home = System.getProperty("user.dir");
-      String resources_dir = project_home + "/src/main/resources";
-      String schema_path = resources_dir + "/schemas/cdm/clinical/";
-      byte[] encoded = Files.readAllBytes(Paths.get(schema_path + file_path));
-      String file_string = new String(encoded, StandardCharsets.UTF_8);
-      JsonArray schema_json_array = JsonParser.parseString(file_string).getAsJsonArray();
+    String jsonString = getJsonString(schema_path + file_path);
+    JsonArray schema_json_array = JsonParser.parseString(jsonString).getAsJsonArray();
 
-      for (Object schema_json_field : schema_json_array) {
-        JsonObject field = (JsonObject) schema_json_field;
+    for (Object schema_json_field : schema_json_array) {
+      JsonObject field = (JsonObject) schema_json_field;
 
-        String name = field.get("name").getAsString();
-        String type = field.get("type").getAsString();
-        Schema.FieldType datatype;
-        switch (type) {
-          case "integer":
-            datatype = Schema.FieldType.INT64;
-            break;
-          case "float":
-            datatype = Schema.FieldType.DOUBLE;
-            break;
-          default:
-            datatype = Schema.FieldType.STRING;
-        }
-        fields.add(Schema.Field.nullable(name, datatype));
+      String name = field.get("name").getAsString();
+      String type = field.get("type").getAsString();
+      Schema.FieldType datatype;
+      switch (type) {
+        case "integer":
+          datatype = Schema.FieldType.INT64;
+          break;
+        case "float":
+          datatype = Schema.FieldType.DOUBLE;
+          break;
+        default:
+          datatype = Schema.FieldType.STRING;
       }
+      fields.add(Schema.Field.nullable(name, datatype));
+    }
+    return Schema.of(fields.toArray(new Schema.Field[0]));
+  }
+
+  public static String getJsonString(String file_path) {
+    byte[] encoded = new byte[0];
+    try {
+      encoded = Files.readAllBytes(Paths.get(schema_path + file_path));
     } catch (IOException e) {
       System.out.println(e.getMessage() + ", could not read schema for file " + file_path);
     }
-    return Schema.of(fields.toArray(new Schema.Field[0]));
+    return new String(encoded, StandardCharsets.UTF_8);
   }
 }
