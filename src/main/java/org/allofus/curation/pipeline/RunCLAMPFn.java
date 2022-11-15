b4ceb941fe4bf6716e08b01c9ea0f2099b5bcd6c
Nishanth Pavinkurve <npp267@nyu.edu>
2022-11-15 17:18:31 +0530

    [DC-2840] Fix paths

--- src/main/java/org/allofus/curation/pipeline/RunCLAMPFn.java
+++ src/main/java/org/allofus/curation/pipeline/RunCLAMPFn.java
@@ -53,13 +53,13 @@ public class RunCLAMPFn extends PTransform<PCollection<Row>, PCollection<Row>> {
     String resources_param = options.getResourcesDir();
     String resources_dir = SanitizeInput.sanitize(resources_param);
     String umlsIndexDir = "/index/umls_index";
-    String pipeline_file = "/pipeline/clinical_pipeline.pipeline.jar";
+    String pipeline_file = "/pipeline/clamp-ner.pipeline.jar";
     List<DocProcessor> pipeline;
 
     if (resources_dir.startsWith("gs")) {
       StorageTmp stmp = new StorageTmp(resources_dir);
-      umlsIndexDir = stmp.StoreTmpDir(resources_dir + "/" + umlsIndexDir.substring(1));
-      pipeline_file = stmp.StoreTmpFile(resources_dir + "/" + pipeline_file.substring(1));
+      umlsIndexDir = stmp.StoreTmpDir(umlsIndexDir.substring(1));
+      pipeline_file = stmp.StoreTmpFile(pipeline_file.substring(1));
     } else {
       umlsIndexDir = resources_dir + umlsIndexDir;
       pipeline_file = resources_dir + pipeline_file;
