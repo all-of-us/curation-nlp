6b5ab5bf48a19a55c46a9ff9ad06ba14cfd06bfb
Nishanth Pavinkurve <npp267@nyu.edu>
2022-11-17 07:31:59 +0530

    [DC-2842] Use correct order

--- src/main/java/org/allofus/curation/pipeline/CurationNLPOptions.java
+++ src/main/java/org/allofus/curation/pipeline/CurationNLPOptions.java
@@ -9,41 +9,35 @@ public interface CurationNLPOptions extends PipelineOptions {
   @Description("Path of the directory/table to read from")
   @Validation.Required
   String getInput();
-
   void setInput(String value);
 
   @Description("Path of the directory containing resources for UMLS index, pipeline jar")
   @Validation.Required
   String getResourcesDir();
-
   void setResourcesDir(String value);
 
   @Description("Input file type, e.g: csv, jsonl, bigquery")
   @Validation.Required
   String getInputType();
+  void setInputType(String value);
 
-  void setPipeline(String value);
-
-  @Description("Pipeline jar to use, e.g: clamp-ner.pipeline.jar, clinical_pipeline.pipeline.jar")
+  @Description(
+    "Pipeline jar to use in resourcesDir/pipeline e.g: clamp-ner.pipeline.jar, clinical_pipeline.pipeline.jar")
   @Validation.Required
   String getPipeline();
-
-  void setInputType(String value);
+  void setPipeline(String value);
 
   @Description("Output file type, e.g: csv, jsonl, bigquery")
   @Validation.Required
   String getOutputType();
-
   void setOutputType(String value);
 
   @Description("Path of the directory/table to write to")
   @Validation.Required
   String getOutput();
-
   void setOutput(String value);
 
   @Description("Build Identifier")
   String getBuildId();
-
   void setBuildId(String BuildId);
 }
