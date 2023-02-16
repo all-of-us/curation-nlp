f088ad0ed31cefdd4a80338c9917f0becddb64c7
Nishanth Pavinkurve <npp267@nyu.edu>
2023-02-16 16:18:03 -0500

    Incremental writes to JSONL for every 60 seconds

--- src/main/java/org/allofus/curation/io/jsonl/JSONLWrite.java
+++ src/main/java/org/allofus/curation/io/jsonl/JSONLWrite.java
@@ -3,15 +3,28 @@ package org.allofus.curation.io.jsonl;
 import org.allofus.curation.io.factory.IOWrite;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.transforms.ToJson;
+import org.apache.beam.sdk.transforms.windowing.*;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.sdk.values.Row;
+import org.joda.time.Duration;
 
 public class JSONLWrite extends IOWrite {
+
+  Integer maxOutputPartitionSeconds = 60;
 
   public PDone expand(PCollection<Row> input) {
     return input
         .apply(ToJson.of())
-        .apply(TextIO.write().to(output_sink).withSuffix("." + output_ext).withNumShards(1));
+        .apply("Windowing",
+          Window.<String>into(FixedWindows.of(Duration.standardSeconds(this.maxOutputPartitionSeconds)))
+            .triggering(Repeatedly.forever(
+              AfterFirst.of(AfterPane.elementCountAtLeast(100),
+                AfterProcessingTime
+                  .pastFirstElementInPane()
+                  .plusDelayOf(Duration.standardSeconds(this.maxOutputPartitionSeconds/4)))))
+            .withAllowedLateness(Duration.standardSeconds(this.maxOutputPartitionSeconds/12))
+            .discardingFiredPanes())
+        .apply(TextIO.write().to(output_sink).withWindowedWrites().withNumShards(1).withSuffix("." + output_ext));
   }
 }
