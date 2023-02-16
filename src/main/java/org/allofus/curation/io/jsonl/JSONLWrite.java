8c4b9a02ed20e8025353fd1e2fa7597bc5fb00fd
Nishanth Pavinkurve <npp267@nyu.edu>
2023-02-16 16:19:58 -0500

    Add output batch size

--- src/main/java/org/allofus/curation/io/jsonl/JSONLWrite.java
+++ src/main/java/org/allofus/curation/io/jsonl/JSONLWrite.java
@@ -12,18 +12,19 @@ import org.joda.time.Duration;
 public class JSONLWrite extends IOWrite {
 
   Integer maxOutputPartitionSeconds = 60;
+  Integer outputBatchSize = 1000;
 
   public PDone expand(PCollection<Row> input) {
     return input
         .apply(ToJson.of())
         .apply("Windowing",
-          Window.<String>into(FixedWindows.of(Duration.standardSeconds(this.maxOutputPartitionSeconds)))
+          Window.<String>into(FixedWindows.of(Duration.standardSeconds(maxOutputPartitionSeconds)))
             .triggering(Repeatedly.forever(
-              AfterFirst.of(AfterPane.elementCountAtLeast(100),
+              AfterFirst.of(AfterPane.elementCountAtLeast(outputBatchSize),
                 AfterProcessingTime
                   .pastFirstElementInPane()
-                  .plusDelayOf(Duration.standardSeconds(this.maxOutputPartitionSeconds/4)))))
-            .withAllowedLateness(Duration.standardSeconds(this.maxOutputPartitionSeconds/12))
+                  .plusDelayOf(Duration.standardSeconds(maxOutputPartitionSeconds/4)))))
+            .withAllowedLateness(Duration.standardSeconds(maxOutputPartitionSeconds/12))
             .discardingFiredPanes())
         .apply(TextIO.write().to(output_sink).withWindowedWrites().withNumShards(1).withSuffix("." + output_ext));
   }
