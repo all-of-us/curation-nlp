57a30b4a55a59a0bf49fb980642e340a3bd12d6f
Nishanth Pavinkurve <np2689@cumc.columbia.edu>
2023-02-21 16:46:41 -0500

    [DC-3092] Stream write on direct runner to JSONL locally (#12)
    * [DC-2821] Add multiple write streams
    
    * [DC-2821] Update versions
    
    * [DC-2821] Update resources
    
    * Add StreamRows to use later
    
    * Use STORAGE_WRITE_API
    
    * Remove print statement
    
    * Add streaming option
    
    * Comment out storage write api
    
    * Incremental writes to JSONL for every 60 seconds
    
    * Add output batch size
    
    * Remove streaming

--- src/main/java/org/allofus/curation/io/jsonl/JSONLWrite.java
+++ src/main/java/org/allofus/curation/io/jsonl/JSONLWrite.java
@@ -3,15 +3,29 @@ package org.allofus.curation.io.jsonl;
 import org.allofus.curation.io.factory.IOWrite;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.transforms.ToJson;
+import org.apache.beam.sdk.transforms.windowing.*;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.sdk.values.Row;
+import org.joda.time.Duration;
 
 public class JSONLWrite extends IOWrite {
+
+  Integer maxOutputPartitionSeconds = 60;
+  Integer outputBatchSize = 1000;
 
   public PDone expand(PCollection<Row> input) {
     return input
         .apply(ToJson.of())
-        .apply(TextIO.write().to(output_sink).withSuffix("." + output_ext).withNumShards(1));
+        .apply("Windowing",
+          Window.<String>into(FixedWindows.of(Duration.standardSeconds(maxOutputPartitionSeconds)))
+            .triggering(Repeatedly.forever(
+              AfterFirst.of(AfterPane.elementCountAtLeast(outputBatchSize),
+                AfterProcessingTime
+                  .pastFirstElementInPane()
+                  .plusDelayOf(Duration.standardSeconds(maxOutputPartitionSeconds/4)))))
+            .withAllowedLateness(Duration.standardSeconds(maxOutputPartitionSeconds/12))
+            .discardingFiredPanes())
+        .apply(TextIO.write().to(output_sink).withWindowedWrites().withNumShards(1).withSuffix("." + output_ext));
   }
 }
