8cfc83b4229f1ca2298b7ab7e6ea6890b6e7c836
Nishanth Pavinkurve <npp267@nyu.edu>
2023-04-25 12:18:32 -0400

    Add Parquet read/write options

--- src/main/java/org/allofus/curation/io/factory/IOWriteFactory.java
+++ src/main/java/org/allofus/curation/io/factory/IOWriteFactory.java
@@ -3,6 +3,7 @@ package org.allofus.curation.io.factory;
 import org.allofus.curation.io.bigquery.BigQueryWrite;
 import org.allofus.curation.io.csv.CSVWrite;
 import org.allofus.curation.io.jsonl.JSONLWrite;
+import org.allofus.curation.io.parquet.ParquetWrite;
 
 public class IOWriteFactory {
   public static IOWrite create(String output_type) {
@@ -10,6 +11,8 @@ public class IOWriteFactory {
       return new CSVWrite();
     } else if ("jsonl".equalsIgnoreCase(output_type)) {
       return new JSONLWrite();
+    } else if ("parquet".equalsIgnoreCase(output_type)) {
+      return new ParquetWrite();
     } else if ("bigquery".equalsIgnoreCase(output_type)) {
       return new BigQueryWrite();
     }
