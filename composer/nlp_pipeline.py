90ed54f6b69dd20e804dd36d4932bd54bad69ac0
jp3477 <jp3477@cumc.columbia.edu>
2024-09-17 11:56:33 -0400

    restructured dag tasks

--- composer/nlp_pipeline.py
+++ composer/nlp_pipeline.py
@@ -27,180 +27,130 @@ def nlp_pipeline():
     NOTES_BQ_DATASET = Variable.get('notes_bq_dataset')
     NOTE_NLP_BQ_DATASET = Variable.get('note_nlp_bq_dataset')
 
-    # export_query = f"""
-    #     EXPORT DATA
-    #     OPTIONS (
-    #         uri='gs://{NOTES_BUCKET_NAME}/{HPO_ID}/input/*.jsonl',
-    #         overwrite=true,
-    #         format='JSON'
-    #     ) AS
-    #     SELECT *
-    #     FROM `{PROJECT_ID}.{NOTES_BQ_DATASET}.{HPO_ID}_note`
-    #     WHERE note_id NOT IN (SELECT note_id
-    #         FROM `{PROJECT_ID}.{NOTES_BQ_DATASET}.{HPO_ID}_note_nlp`
-    #     )
-    #     LIMIT 50
+    @task()
+    def export_notes_to_storage():
+        export_query = f"""
+            #standardSQL
+            EXPORT DATA
+            OPTIONS (
+                uri='gs://{NOTES_BUCKET_NAME}/{HPO_ID}/input/*.jsonl',
+                overwrite=true,
+                format='JSON'
+            ) AS
+            SELECT *
+            FROM `{PROJECT_ID}.{NOTES_BQ_DATASET}.{HPO_ID}_note`
+            WHERE note_id NOT IN (SELECT note_id
+                FROM `{PROJECT_ID}.{NOTES_BQ_DATASET}.{HPO_ID}_note_nlp`
+            )
+            LIMIT 50
 
-    # """
+        """.strip()
 
-    # export_notes_to_storage = BigQueryInsertJobOperator(
-    #     task_id='export_notes_to_storage',
-    #     configuration={
-    #         'query': {
-    #             'query':
-    #             f"SELECT * FROM {PROJECT_ID}.{NOTES_BQ_DATASET}.{HPO_ID}_note LIMIT 100 ",
-    #             'useLegacySql': False,
-    #             'timeoutMs': 5000
-    #         }
-    #     },
-    # )
+        hook = bigquery.BigQueryHook(gcp_conn_id='google_cloud_default',
+                                     use_legacy_sql=False)
 
-    bash_check = BashOperator(
-        task_id="bash_check",
-        bash_command="echo 1",
-    )
-
-    export_notes_to_storage = BigQueryGetDatasetTablesOperator(
-        task_id="export_notes_to_storage",
-        dataset_id='composer_test',
-        gcp_conn_id='google_cloud_default')
-
-    # @task
-    # def export_notes_to_storage():
-    #     logging.info('Starting task')
-    #     hook = bigquery.BigQueryHook(gcp_conn_id='google_cloud_default',
-    #                                  use_legacy_sql=False)
-
-    #     logging.info('Getting tables list')
-    #     tables = hook.get_dataset_tables_list(
-    #         'composer_dataset_name', project_id='{PROJECT_ID}')
-    #     logging.info('Finish getting tables list')
-
-    # @task()
-    # def export_notes_to_storage():
-    # export_query = f"""
-    #     EXPORT DATA
-    #     OPTIONS (
-    #         uri='gs://{NOTES_BUCKET_NAME}/{HPO_ID}/input/*.jsonl',
-    #         overwrite=true,
-    #         format='JSON'
-    #     ) AS
-    #     SELECT *
-    #     FROM `{PROJECT_ID}.{NOTES_BQ_DATASET}.{HPO_ID}_note`
-    #     WHERE note_id NOT IN (SELECT note_id
-    #         FROM `{PROJECT_ID}.{NOTES_BQ_DATASET}.{HPO_ID}_note_nlp`
-    #     )
-    #     LIMIT 50
-
-    # """
-
-    #     # export_query = f"SELECT * FROM {PROJECT_ID}.{NOTES_BQ_DATASET}.{HPO_ID}_note LIMIT 100 "
-    #     export_query = f"SELECT 1 "
-
-    #     hook = bigquery.BigQueryHook(gcp_conn_id='google_cloud_default',
-    #                                  use_legacy_sql=False)
-
-    #     logging.info(
-    #         f'Exporting {HPO_ID} notes to gs://{NOTES_BUCKET_NAME}/{HPO_ID}/input/*.jsonl.'
-    #     )
-    #     logging.info(export_query)
-    #     # hook.run_query(export_query)
-    #     hook.insert_job(
-    #         configuration={'query': {
-    #             'query': export_query,
-    #             'timeoutMs': 1
-    #         }},
-    #         job_id='export_notes')
-    #     logging.info(f'Export complete.')
+        logging.info(
+            f'Exporting {HPO_ID} notes to gs://{NOTES_BUCKET_NAME}/{HPO_ID}/input/*.jsonl.'
+        )
+        logging.info(export_query)
+        # hook.run_query(export_query)
+        hook.insert_job(
+            configuration={
+                'query': {
+                    'query': export_query,
+                    'useLegacySQL': False,
+                    # 'timeoutMs': 1
+                }
+            },
+            # job_id='export_notes'
+        )
+        logging.info(f'Export complete.')
 
     jar_path = f"gs://{CLAMP_BUCKET_NAME}/{CLAMP_JAR_NAME}"
 
-    # run_clamp = BeamRunJavaPipelineOperator(
-    #     runner=BeamRunnerType.DataflowRunner,
-    #     task_id='clamp-job',
-    #     jar=jar_path,
-    #     job_class='org.allofus.curation.pipeline.CurationNLPMain',
-    #     dataflow_config={
-    #         'job_name': 'composer_run_clamp',
-    #         'wait_until_finished': True
-    #     },
-    #     pipeline_options={
-    #         'gcpTempLocation': f"gs://{DATAFLOW_BUCKET_NAME}/gcp_tmp",
-    #         'stagingLocation': f"gs://{DATAFLOW_BUCKET_NAME}/staging",
-    #         'tempLocation': f"gs://{DATAFLOW_BUCKET_NAME}/tmp",
-    #         'project': PROJECT_ID,
-    #         'usePublicIps': False,
-    #         'region': 'us-central1',
-    #         'numWorkers': 100,
-    #         'maxNumWorkers': 100,
-    #         'numberOfWorkerHarnessThreads': 2,
-    #         'workerMachineType': 'custom-8-16384',
-    #         'diskSizeGb': 50,
-    #         'experiments': 'use_runner_v2',
-    #         'subnetwork': SUBNETWORK,
-    #         'input': f"gs://{NOTES_BUCKET_NAME}/{HPO_ID}/input/",
-    #         'output': f"gs://{NOTES_BUCKET_NAME}/{HPO_ID}/output/",
-    #         'pipeline': 'clinical_pipeline.pipeline.jar',
-    #         'maxClampThreads': 10,
-    #         'maxOutputPartitionSeconds': 1800,
-    #         'maxOutputBatchSize': 100000,
-    #         'resourcesDir': f"gs://{DATAFLOW_BUCKET_NAME}/resources",
-    #         'inputType': 'jsonl',
-    #         'outputType': 'jsonl',
-    #         'streaming': True,
-    #         'enableStreamingEngine': True
-    #     })
+    run_clamp = BeamRunJavaPipelineOperator(
+        runner=BeamRunnerType.DataflowRunner,
+        task_id='clamp-job',
+        jar=jar_path,
+        job_class='org.allofus.curation.pipeline.CurationNLPMain',
+        dataflow_config={
+            'job_name': 'composer_run_clamp',
+            'wait_until_finished': True
+        },
+        pipeline_options={
+            'gcpTempLocation': f"gs://{DATAFLOW_BUCKET_NAME}/gcp_tmp",
+            'stagingLocation': f"gs://{DATAFLOW_BUCKET_NAME}/staging",
+            'tempLocation': f"gs://{DATAFLOW_BUCKET_NAME}/tmp",
+            'project': PROJECT_ID,
+            'usePublicIps': False,
+            'region': 'us-central1',
+            'numWorkers': 100,  # Use 10
+            'maxNumWorkers': 100,  # Use 10
+            'numberOfWorkerHarnessThreads': 2,
+            'workerMachineType': 'custom-8-16384',
+            'diskSizeGb': 50,
+            'experiments': 'use_runner_v2',
+            'subnetwork': SUBNETWORK,
+            'input': f"gs://{NOTES_BUCKET_NAME}/{HPO_ID}/input/",
+            'output': f"gs://{NOTES_BUCKET_NAME}/{HPO_ID}/output/",
+            'pipeline': 'clinical_pipeline.pipeline.jar',
+            'maxClampThreads': 10,
+            'maxOutputPartitionSeconds': 1800,
+            'maxOutputBatchSize': 100000,
+            'resourcesDir': f"gs://{DATAFLOW_BUCKET_NAME}/resources",
+            'inputType': 'jsonl',
+            'outputType': 'jsonl',
+            'streaming': True,
+            'enableStreamingEngine': True
+        })
 
-    # @task()
-    # def import_note_nlp_to_bigquery():
-    #     import_query = f"""
-    #         LOAD DATA INTO `{PROJECT_ID}.{NOTE_NLP_BQ_DATASET}.{HPO_ID}_note_nlp` (
-    #         note_nlp_id INT64,
-    #         note_id INT64,
-    #         section_concept_id INT64,
-    #         snippet STRING,
-    #         offset STRING,
-    #         lexical_variant STRING,
-    #         note_nlp_concept_id INT64,
-    #         note_nlp_source_concept_id INT64,
-    #         nlp_system STRING,
-    #         nlp_date DATE,
-    #         nlp_datetime TIMESTAMP,
-    #         term_exists STRING,
-    #         term_temporal STRING,
-    #         term_modifiers STRING
-    #         )
-    #         FROM FILES (
-    #         format='JSON',
-    #         uris = ['gs://{NOTES_BUCKET_NAME}/{HPO_ID}/output/*']
-    #         )
-    #     """
+    @task()
+    def import_note_nlp_to_bigquery():
+        import_query = f"""
+            #standardSQL
+            LOAD DATA INTO `{PROJECT_ID}.{NOTE_NLP_BQ_DATASET}.{HPO_ID}_note_nlp` (
+            note_nlp_id INT64,
+            note_id INT64,
+            section_concept_id INT64,
+            snippet STRING,
+            offset STRING,
+            lexical_variant STRING,
+            note_nlp_concept_id INT64,
+            note_nlp_source_concept_id INT64,
+            nlp_system STRING,
+            nlp_date DATE,
+            nlp_datetime TIMESTAMP,
+            term_exists STRING,
+            term_temporal STRING,
+            term_modifiers STRING
+            )
+            FROM FILES (
+            format='JSON',
+            uris = ['gs://{NOTES_BUCKET_NAME}/{HPO_ID}/output/*']
+            )
+        """
 
-    #     hook = bigquery.BigQueryHook(gcp_conn_id='google_cloud_default',
-    #                                  use_legacy_sql=False)
+        hook = bigquery.BigQueryHook(gcp_conn_id='google_cloud_default',
+                                     use_legacy_sql=False)
 
-    #     logging.info(
-    #         f'Importing {HPO_ID} note_nlp records to {PROJECT_ID}.{NOTE_NLP_BQ_DATASET}.{HPO_ID}_note_nlp.'
-    #     )
-    #     # hook.run_query(import_query)
+        logging.info(
+            f'Importing {HPO_ID} note_nlp records to {PROJECT_ID}.{NOTE_NLP_BQ_DATASET}.{HPO_ID}_note_nlp.'
+        )
+        # hook.run_query(import_query)
 
-    #     hook.insert_job(configuration={
-    #         'query': {
-    #             'query': import_query,
-    #             'timeoutMs': 60000
-    #         }
-    #     },
-    #                     job_id='import_note_nlp')
+        hook.insert_job(configuration={
+            'query': {
+                'query': import_query,
+                'useLegacySQL': False,
+                'timeoutMs': 60000
+            }
+        },
+                        # job_id='import_note_nlp'
+                        )
 
-    #     logging.info(f'Import complete.')
+        logging.info(f'Import complete.')
 
-    # bash_check >> export_notes_to_storage >> run_clamp >> import_note_nlp_to_bigquery(
-    # )
-
-    bash_check >> export_notes_to_storage
+    export_notes_to_storage() >> run_clamp >> import_note_nlp_to_bigquery()
 
 
 nlp_pipeline_dag = nlp_pipeline()
-
-if __name__ == "__main__":
-    nlp_pipeline_dag.test()
